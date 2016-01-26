36b866de6b8aae52b76f6aaeccaeaf333b94a394
Vikraman Choudhury <git@vikraman.org>
2016-01-26 09:53:55 -0500

    Use strict version of modifyIORef

--- Main.hs
+++ Main.hs
@@ -10,7 +10,7 @@ import System.IO.Unsafe
 --   rnf a = a `seq` ()
 
 instance NFData a => NFData (IORef a) where
-  rnf a = unsafePerformIO $ modifyIORef a force
+  rnf a = unsafePerformIO $ modifyIORef' a force
 
 test1 :: IO ()
 test1 = do c <- newCompact 64 (42 :: Int)
