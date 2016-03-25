c9bed365d239b5db18515d27cd9e1e359793f7e8
Vikraman Choudhury <git@vikraman.org>
2016-03-25 13:24:15 -0400

    Initial version for the Data.CNFRef2 api

--- Data/CNFRef2.hs
+++ Data/CNFRef2.hs
@@ -2,8 +2,8 @@
 
 module Data.CNFRef2
        ( CNFRef(..) -- Transparent for now...
-       , unsafeGetCompact
        , DeepStrict (..)
+       , copyToCompact
        , newCNFRef
        , readCNFRef
        , writeCNFRef
@@ -22,34 +22,43 @@ instance NFData a => NFData (IORef a) where
 
 newtype CNFRef s a = CNFRef (Compact s (IORef a))
 
-
 -- | A hypothetical typeclass to track transitive strictness.
 --   I.e. datatypes whose values cannot contain (reach) a thunk.
 class NFData a => DeepStrict a where
 
 instance DeepStrict Int
 
--- | TODO: Replace this with a function that just extracts the "Block", not a Compact.
---   Or at the very least cast it so it returns `Compact s ()`
-unsafeGetCompact :: CNFRef s a -> Compact s (IORef a)
-unsafeGetCompact (CNFRef c) = c
-
+-- | Copy a new value to the same compact region as the CNFRef.
+copyToCompact :: DeepStrict a => CNFRef s a -> a -> IO (Compact s a)
+copyToCompact (CNFRef !c) !a = appendCompact c a  -- This will leak if
+                                                  -- a is not unboxed.
 
 -- | Copy a boxed value into a compact region and create a new CNFRef
 -- that points to it.
 newCNFRef :: DeepStrict a => a -> IO (CNFRef s a)
-newCNFRef c = undefined
+newCNFRef !a = do
+  !ref <- newIORef a
+  -- what's the correct size?
+  let sz = 4096
+  !c <- newCompact sz ref
+  return $! CNFRef c
 
 -- | Read the contents of the CNFRef, but don't lose track of the fact
---   that the value ilives in the same compact region as the reference
+--   that the value lives in the same compact region as the reference
 --   that points to it.
 readCNFRef :: DeepStrict a => CNFRef s a -> IO (Compact s a)
-readCNFRef (CNFRef c) = undefined
+readCNFRef (CNFRef !c) = do
+  let !ref = getCompact c
+  !a <- readIORef ref
+  appendCompact c a
 
 -- | Point a CNFRef at a new value that already lives in the correct
 -- region.
 writeCNFRef :: CNFRef s a -> Compact s a -> IO ()
-writeCNFRef (CNFRef !c) x = undefined
+writeCNFRef (CNFRef !c) !c' = do
+  let !ref = getCompact c
+      !a = getCompact c'
+  writeIORef ref a
 
 -- modifyCNFRef :: DeepStrict a => CNFRef s a -> (a -> Compact s a) -> IO ()
 -- modifyCNFRef !ref !f = undefined
