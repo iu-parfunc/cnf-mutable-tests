75d0123bf4faaf200d4f28002f19b78174f10008
Vikraman Choudhury <git@vikraman.org>
2016-03-24 23:27:41 -0400

    Add an initial version of Data.Compact.Indexed

--- deps/compact-fake/Data/Compact/Indexed.hs
+++ deps/compact-fake/Data/Compact/Indexed.hs
@@ -1,31 +1,41 @@
+{-# LANGUAGE BangPatterns #-}
+
 -- | A version of the (fake) API indexed by a session/region type parameter.
 
-module Data.Compact.Indexed where
+module Data.Compact.Indexed (
+    Compact,
+    getCompact,
+    inCompact,
+    isCompact,
+    newCompact,
+    newCompactNoShare,
+    appendCompact,
+    appendCompactNoShare,
+    ) where
 
-import Control.DeepSeq
+import           Control.DeepSeq
+import qualified Data.Compact    as C
 
--- | The dummy compact type.
-newtype Compact s a = Compact a
+-- | The indexed compact type.
+newtype Compact s a = Compact (C.Compact a)
 
 appendCompact :: NFData a => Compact s b -> a -> IO (Compact s a)
-appendCompact = undefined
+appendCompact (Compact !c) !a = Compact <$> C.appendCompact c a
 
 appendCompactNoShare :: NFData a => Compact s b -> a -> IO (Compact s a)
-appendCompactNoShare = undefined
+appendCompactNoShare (Compact !c) !a = Compact <$> C.appendCompactNoShare c a
 
 getCompact :: Compact s a -> a
-getCompact = undefined
+getCompact (Compact !c) = C.getCompact c
 
--- | This one is hard to fake....
 inCompact :: Compact s a -> b -> Bool
-inCompact = error "inCompact: cannot implement in this dummy implementation"
+inCompact (Compact !c) !b = C.inCompact c b
 
--- | This one too
 isCompact :: a -> Bool
-isCompact = error "isCompact: cannot implement in this dummy implementation"
+isCompact = C.isCompact
 
 newCompact :: NFData a => Word -> a -> IO (Compact s a)
-newCompact = undefined
+newCompact !w !a = Compact <$> C.newCompact w a
 
 newCompactNoShare :: NFData a => Word -> a -> IO (Compact s a)
-newCompactNoShare = undefined
+newCompactNoShare !w !a = Compact <$> C.newCompactNoShare w a
