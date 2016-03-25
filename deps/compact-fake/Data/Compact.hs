ab4d9e3d9b9416679aec9fcbe9ee01be047a00e6
Vikraman Choudhury <git@vikraman.org>
2016-03-24 23:27:27 -0400

    Re-export Data.Compact in compact-fake

--- deps/compact-fake/Data/Compact.hs
+++ deps/compact-fake/Data/Compact.hs
@@ -1,35 +1,8 @@
+{-# LANGUAGE PackageImports #-}
+
 -- | A fake version of the real Data.Compact library that exposes the same interface.
 
-module Data.Compact where
+-- Re-export the ghc "compact" library.
+module Data.Compact (module C) where
 
-import Control.DeepSeq
-
--- | The dummy compact type.
-newtype Compact a = Compact a
-
-appendCompact ::
-  NFData a => Compact b -> a -> IO (Compact a)
-appendCompact = undefined
-
-appendCompactNoShare ::
-  NFData a => Compact b -> a -> IO (Compact a)
-appendCompactNoShare = undefined
-
-getCompact :: Compact a -> a
-getCompact = undefined
-
--- | This one is hard to fake....
-inCompact :: Compact a -> b -> Bool
-inCompact = error "inCompact: cannot implement in this dummy implementation"
-
--- | This one too
-isCompact :: a -> Bool
-isCompact = error "isCompact: cannot implement in this dummy implementation"
-
-newCompact ::
-  NFData a => Word -> a -> IO (Compact a)
-newCompact = undefined
-
-newCompactNoShare ::
-  NFData a => Word -> a -> IO (Compact a)
-newCompactNoShare = undefined
+import "compact" Data.Compact as C
