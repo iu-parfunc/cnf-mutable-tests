eeae394471b9d004dcdb9849cedbb29537107cfc
Ryan Newton <rrnewton@gmail.com>
2016-03-25 07:52:40 -0400

    Create a compact-indexed library out of what was there.  Update stack.yaml

--- deps/compact-fake/Data/Compact.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE PackageImports #-}
-
--- | A fake version of the real Data.Compact library that exposes the same interface.
-
--- Re-export the ghc "compact" library.
-module Data.Compact (module C) where
-
-import "compact" Data.Compact as C
