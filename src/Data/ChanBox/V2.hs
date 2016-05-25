b5129868abb6776d9b1254613196b828118e3392
Vikraman Choudhury <git@vikraman.org>
2016-05-25 03:34:41 -0400

    Refactor ChanBox V2

--- src/Data/ChanBox/V2.hs
+++ src/Data/ChanBox/V2.hs
@@ -1,30 +1,23 @@
 {-# LANGUAGE DeriveAnyClass            #-}
 {-# LANGUAGE DeriveGeneric             #-}
 {-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleInstances         #-}
 {-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE NamedFieldPuns            #-}
-{-# LANGUAGE Rank2Types                #-}
 {-# LANGUAGE RecordWildCards           #-}
 {-# LANGUAGE StandaloneDeriving        #-}
 {-# LANGUAGE Strict                    #-}
 {-# LANGUAGE StrictData                #-}
-{-# LANGUAGE TypeSynonymInstances      #-}
 
 -- |
 
 module Data.ChanBox.V2 where
 
-import           Control.DeepSeq
-import           Control.Monad
-import           Control.Monad.Trans
-import           Data.CNFRef
-import           Data.CNFRef.DeepStrict
-import           Data.Compact.Indexed
-import qualified Data.Map.Strict             as Map
-import           Data.Vector.Unboxed.Mutable as V
-import           Data.Word
-import           GHC.Generics
+import Control.DeepSeq
+import Control.Monad
+import Data.CNFRef
+import Data.CNFRef.DeepStrict
+import Data.Compact.Indexed
+import Data.Vector.Unboxed.Mutable as V
+import GHC.Generics
 
 type Msg = IOVector Int
 
@@ -34,98 +27,115 @@ data MsgList s where
 
 instance NFData a => NFData (Compact s a) where
   rnf _ = ()
+instance DeepStrict a => DeepStrict (Compact s a) where
 
 deriving instance Generic (MsgList s)
 deriving instance NFData (MsgList s)
 deriving instance DeepStrict (MsgList s)
 
-data Chan s = Chan { front :: CNFRef s (MsgList s), rear :: CNFRef s (MsgList s) }
+data Chan s = Chan { block :: BlockChain s, front :: MsgList s, rear :: MsgList s }
   deriving (Generic, NFData, DeepStrict)
 
-data ChanBox = forall s. ChanBox { box :: Chan s, free :: Chan s }
+type ChanRef s = CNFRef s (Chan s)
+
+data ChanBox = forall s. ChanBox { box :: ChanRef s, free :: ChanRef s }
 
 newMessage :: ChanBox -> Int -> IO Msg
-newMessage (ChanBox _ (Chan front _)) n = do
-  c <- readCNFRef front
-  case getCompact c of
+newMessage ChanBox { .. } n = do
+  chan@(Chan _ front _) <- getCompact <$> readCNFRef free
+  case front of
     Nil _ -> V.replicate 1024 n
     Cons msg ref -> do
       let vec = getCompact msg
       forM_ [0 .. 1023] $ V.write vec n
-      p <- readCNFRef ref
-      writeCNFRef front p
+      chan' <- tailChan chan
+      c <- newCompactIn ref chan'
+      writeCNFRef free c
       return vec
 
 newBox :: IO ChanBox
 newBox = runCIO $ do
   bl <- getBlockChain
-  box <- newCNFRef (Nil bl)
-  free <- newCNFRef (Nil bl)
-  return $ ChanBox (Chan box box) (Chan free free)
+  boxRef <- newCNFRef $ Chan bl (Nil bl) (Nil bl)
+  freeRef <- newCNFRef $ Chan bl (Nil bl) (Nil bl)
+  return $ ChanBox boxRef freeRef
 
-lengthMsgList :: MsgList s -> IO Int
-lengthMsgList ls =
-  case ls of
-    Nil _ -> return 0
-    Cons _ ref -> do
-      c <- readCNFRef ref
-      l <- lengthMsgList $ getCompact c
-      return $ l + 1
+lengthChan :: Chan s -> IO Int
+lengthChan Chan { .. } = go front
+  where
+    go p =
+      case p of
+        Nil _ -> return 0
+        Cons _ ref -> do
+          c <- readCNFRef ref
+          l <- go (getCompact c)
+          return $ l + 1
 
-headMsgList :: MsgList s -> IO (Compact s Msg)
-headMsgList ls =
-  case ls of
+sizeBox :: ChanBox -> IO Int
+sizeBox ChanBox { .. } = do
+  boxChan <- getCompact <$> readCNFRef box
+  freeChan <- getCompact <$> readCNFRef free
+  liftM2 (+) (lengthChan boxChan) (lengthChan freeChan)
+
+headChan :: Chan s -> IO (Compact s Msg)
+headChan Chan { .. } =
+  case front of
     Nil _      -> error "head on empty list"
     Cons msg _ -> return msg
 
-tailMsgList :: MsgList s -> IO (MsgList s)
-tailMsgList ls =
-  case ls of
-    Nil _      -> error "tail on empty list"
-    Cons _ ref -> getCompact <$> readCNFRef ref
+tailChan :: Chan s -> IO (Chan s)
+tailChan Chan { .. } =
+  case front of
+    Nil _ -> error "tail on empty list"
+    Cons _ ref -> do
+      p <- getCompact <$> readCNFRef ref
+      return $ Chan block p rear
 
-lengthChan :: Chan s -> IO Int
-lengthChan Chan { .. } = do
-  c <- readCNFRef front
-  lengthMsgList $ getCompact c
+consChan :: Compact s Msg -> ChanRef s -> IO ()
+consChan msg ref = do
+  Chan block front rear <- getCompact <$> readCNFRef ref
+  p <- newCNFRefIn block front
+  let front' = Cons msg p
+      rear' =
+        case rear of
+          Nil _ -> front
+          r     -> r
+  c <- newCompactIn ref $ Chan block front' rear'
+  writeCNFRef ref c
 
-sizeBox :: ChanBox -> IO Int
-sizeBox ChanBox { .. } =
-  liftM2 (+) (lengthChan box) (lengthChan free)
+snocChan :: ChanRef s -> Compact s Msg -> IO ()
+snocChan ref msg = do
+  Chan block front rear <- getCompact <$> readCNFRef ref
+  p <- newCNFRefIn block (Nil block)
+  let rear' = Cons msg p
+      front' =
+        case front of
+          Nil _ -> rear'
+          f     -> f
+  case rear of
+    Nil _ -> do
+      c' <- newCompactIn ref $ Chan block front' rear'
+      writeCNFRef ref c'
+    Cons _ p -> do
+      c' <- newCompactIn p rear'
+      writeCNFRef p c'
 
 maxLengthChan :: Int
 maxLengthChan = 200000
 
 dropMinChan :: ChanBox -> IO ()
 dropMinChan ChanBox { .. } = do
-  c <- readCNFRef $ front box
-  let boxMsgs = getCompact c
-  l <- lengthMsgList boxMsgs
+  boxChan <- getCompact <$> readCNFRef box
+  l <- lengthChan boxChan
   when (l >= maxLengthChan) $ do
-    v <- headMsgList boxMsgs
-    boxMsgs' <- tailMsgList boxMsgs
-    let freeMsgs' = Cons v $ front free
-    c' <- appendCompactNoShare c freeMsgs'
-    writeCNFRef (front free) c'
-    c' <- appendCompactNoShare c boxMsgs'
-    writeCNFRef (front box) c'
+    v <- headChan boxChan
+    boxChan' <- tailChan boxChan
+    c <- newCompactIn box boxChan'
+    writeCNFRef box c
+    consChan v free
 
 pushMsg :: ChanBox -> Msg -> IO ()
 pushMsg b@ChanBox { .. } msg = do
-  -- FIXME:
-  bl <- newCompactIn (rear box) ()
-  c <- appendCompactNoShare bl msg
-  p <- readCNFRef (rear box)
-  case getCompact p of
-    Nil bl -> do
-      let rear' = Cons c (rear box)
-      c' <- appendCompactNoShare bl rear'
-      writeCNFRef (front box) c'
-      writeCNFRef (rear box) c'
-    Cons msg ref -> do
-      -- dropMinChan b
-      ref' <- newCNFRefIn' (rear box) (Nil bl)
-      let rear' = Cons c ref'
-      c' <- appendCompactNoShare bl rear'
-      writeCNFRef ref c'
-      writeCNFRef (rear box) c'
+  -- dropMinChan b
+  c <- newCompactIn box msg
+  snocChan box c
