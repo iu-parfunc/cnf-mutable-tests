c4242b14ced033538840dae13d2b74f5b6ed1daf
Vikraman Choudhury <git@vikraman.org>
2016-05-31 16:50:50 -0400

    Rewrite ChanBox.V1 to be like a ring buffer

--- src/Data/ChanBox/V1.hs
+++ src/Data/ChanBox/V1.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DeriveAnyClass            #-}
+{-# LANGUAGE DeriveGeneric             #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE NamedFieldPuns            #-}
 {-# LANGUAGE RecordWildCards           #-}
 {-# LANGUAGE Strict                    #-}
 {-# LANGUAGE StrictData                #-}
@@ -10,6 +13,7 @@ module Data.ChanBox.V1 where
 
 import Control.DeepSeq
 import Control.Monad               as M
+import Control.Monad.Trans
 import Data.CNFRef
 import Data.CNFRef.DeepStrict
 import Data.Compact.Indexed
@@ -19,63 +23,72 @@ import GHC.Generics
 
 type Msg = IOVector Int
 
-type Chan s = CNFRef s (Vector Msg)
+instance DeepStrict a => DeepStrict (Vector a)
 
-instance DeepStrict (Vector Msg) where
+data Chan s = Chan { vec   :: Compact s (Vector Msg)
+                   , front :: CNFRef s Int
+                   , rear  :: CNFRef s Int
+                   }
+  deriving (Generic, NFData, DeepStrict)
 
-data ChanBox = forall s. ChanBox { box :: Chan s, free :: Chan s }
+data ChanBox = forall s. ChanBox { box :: Chan s }
 
 newMessage :: ChanBox -> Int -> IO Msg
-newMessage ChanBox { .. } n = do
-  c <- readCNFRef free
-  let vec = getCompact c
-  case B.length vec of
-    0 -> V.replicate 1024 n
-    _ -> do
-      let msg = B.head vec
-      M.forM_ [0 .. 1023] $ V.write msg n
-      c' <- appendCompactNoShare c $ B.tail vec
-      writeCNFRef free c'
-      return msg
+newMessage ChanBox { .. } n = V.replicate 1024 n
+
+-- FIXME: CNF crashes with large size
+maxLengthChan :: Int
+maxLengthChan = 20000
+
+(|+|) :: Int -> Int -> Int
+x |+| y = (x + y) `mod` (maxLengthChan - 1)
 
 newBox :: IO ChanBox
 newBox = runCIO $ do
-  box <- newCNFRef empty
-  free <- newCNFRef empty
-  return $ ChanBox box free
+  front <- newCNFRef 0
+  rear <- newCNFRef 0
+  msg <- V.replicate 1024 0
+  let vec = B.replicate maxLengthChan msg
+  c <- liftIO $ newCompactIn front vec
+  let box = Chan c front rear
+  return $ ChanBox box
 
 lengthChan :: Chan s -> IO Int
-lengthChan chan =
-  B.length . getCompact <$> readCNFRef chan
+lengthChan Chan { .. } = do
+  start <- getCompact <$> readCNFRef front
+  end <- getCompact <$> readCNFRef rear
+  return $ if end >= start
+             then end - start
+             else end - start + maxLengthChan + 1
 
 sizeBox :: ChanBox -> IO Int
-sizeBox ChanBox { .. } =
-  liftM2 (+) (lengthChan box) (lengthChan free)
-
-maxLengthChan :: Int
-maxLengthChan = 200000
+sizeBox ChanBox { .. } = lengthChan box
 
 dropMinChan :: ChanBox -> IO ()
-dropMinChan ChanBox { .. } = do
-  c <- readCNFRef box
-  c' <- readCNFRef free
-  let boxVec = getCompact c
-      freeVec = getCompact c'
-      l = B.length boxVec
-  when (l >= maxLengthChan) $ do
-    let v = B.head boxVec
-        boxVec' = B.tail boxVec
-        freeVec' = cons v freeVec
-    c' <- appendCompactNoShare c freeVec'
-    writeCNFRef free c'
-    c' <- appendCompactNoShare c boxVec'
-    writeCNFRef box c'
+dropMinChan ChanBox { .. } =
+  case box of
+    Chan { .. } -> do
+      start <- getCompact <$> readCNFRef front
+      -- end <- getCompact <$> readCNFRef rear
+      let start' = start |+| 1
+      c <- newCompactIn front start'
+      writeCNFRef front c
 
 pushMsg :: ChanBox -> Msg -> IO ()
-pushMsg b@ChanBox { .. } msg = do
-  dropMinChan b
-  c <- readCNFRef box
-  let boxVec = getCompact c
-      boxVec' = snoc boxVec msg
-  c' <- appendCompactNoShare c boxVec'
-  writeCNFRef box c'
+pushMsg b@ChanBox { .. } msg =
+  case box of
+    Chan { .. } -> do
+      sz <- lengthChan box
+      when (sz == maxLengthChan) $ dropMinChan b
+      -- start <- getCompact <$> readCNFRef front
+      end <- getCompact <$> readCNFRef rear
+      let end' = end |+| 1
+          v = getCompact vec
+          msg' = v ! end
+      M.forM_ [0 .. 1023] $ \i -> do
+        a <- unsafeRead msg i
+        c <- appendCompactNoShare vec a
+        -- FIXME: unsafe interface
+        unsafeWrite msg' i (getCompact c)
+      c <- newCompactIn rear end'
+      writeCNFRef rear c
