cb120b9f79b1729db0dfd65fd933bb6c74131490
Vikraman Choudhury <git@vikraman.org>
2016-05-31 19:39:29 -0400

    Rewrite benchmark to use iterations with criterion

--- bench/BenchChanBox.hs
+++ bench/BenchChanBox.hs
@@ -22,56 +22,60 @@ import qualified Data.ChanBox.V0 as CB0
 import qualified Data.ChanBox.V1 as CB1
 import qualified Data.ChanBox.V2 as CB2
 
-data Env = Env { n          :: Int
-               , range      :: Int
-               , size       :: Int
-               , criterion  :: [String]
-               , randomInts :: Vector Int
-               }
+data Flags = Flags { size       :: Int
+                   , range      :: Int
+                   , randomSize :: Int
+                   , criterion  :: [String]
+                   }
   deriving (Eq, Show, Data, Typeable, Generic, NFData)
 
-defaultEnv :: Env
-defaultEnv = Env
-  { n = (10 ^ 3) &= name "N" &= help "Number of messages"
+data Env = Env { randomInts :: VU.Vector Int
+               , cb0        :: CB0.ChanBox
+               , cb1        :: CB1.ChanBox
+               }
+  deriving (Generic, NFData)
+
+defaultFlags :: Flags
+defaultFlags = Flags
+  { size = (10 ^ 3) &= name "size" &= help "Maximum size of ChanBox"
   , range = (2 ^ 63 - 1) &= help "Integer range for messages"
-  , size = (10 ^ 7) &= help "Size of precomputed random vector"
+  , randomSize = (10 ^ 7) &= help "Size of precomputed random vector"
   , criterion = [] &= help "Arguments to criterion"
-  , randomInts = empty &= ignore
   } &= CA.verbosity &= program "bench-chanbox"
 
-setupEnv :: Env -> IO Env
-setupEnv env@Env { .. } = do
+setupEnv :: Flags -> IO Env
+setupEnv Flags { .. } = do
   gen <- PCG.createSystemRandom
-  randomInts <- VU.replicateM size (uniformR (0, range) gen :: IO Int)
-  return $ env { randomInts = randomInts }
+  randomInts <- VU.replicateM randomSize (uniformR (0, range) gen :: IO Int)
+  cb0 <- CB0.newBox
+  cb1 <- CB1.newBox' size
+  return $ Env { randomInts = randomInts, cb0 = cb0, cb1 = cb1 }
 
 config :: Config
 config = defaultConfig { reportFile = Just "report.html"
                        , csvFile = Just "report.csv"
                        }
 
-cbv0Bench :: Env -> IO ()
-cbv0Bench Env { .. } = do
-  cb <- CB0.newBox
-  for_ 0 n $ \i -> do
-    msg <- CB0.newMessage cb $ randomInts ! (i `mod` size)
-    CB0.pushMsg cb msg
+cbv0Bench :: Flags -> Env -> Benchmarkable
+cbv0Bench Flags { .. } Env { .. } = Benchmarkable $
+  \n -> for_ 0 (fromIntegral n) $ \i -> do
+    msg <- CB0.newMessage cb0 $ randomInts ! (i `mod` randomSize)
+    CB0.pushMsg cb0 msg
 
-cbv1Bench :: Env -> IO ()
-cbv1Bench Env { .. } = do
-  cb <- CB1.newBox
-  for_ 1 n $ \i -> do
-    msg <- CB1.newMessage cb $ randomInts ! (i `mod` size)
-    CB1.pushMsg cb msg
+cbv1Bench :: Flags -> Env -> Benchmarkable
+cbv1Bench Flags { .. } Env { .. } = Benchmarkable $
+  \n -> for_ 1 (fromIntegral n) $ \i -> do
+    msg <- CB1.newMessage cb1 $ randomInts ! (i `mod` randomSize)
+    CB1.pushMsg cb1 msg
 
 main :: IO ()
 main = do
-  args@Env { .. } <- cmdArgs defaultEnv
+  flags@Flags { .. } <- cmdArgs defaultFlags
   withArgs criterion $
     defaultMainWith config
-      [ env (setupEnv args) $ \env ->
+      [ env (setupEnv flags) $ \env ->
         bgroup "ChanBox"
-          [ bgroup "Data.ChanBox.V0" [bench (show n) $ nfIO (cbv0Bench env)]
-          , bgroup "Data.ChanBox.V1" [bench (show n) $ nfIO (cbv1Bench env)]
+          [ bench "Data.ChanBox.V0" $ cbv0Bench flags env
+          , bench "Data.ChanBox.V1" $ cbv1Bench flags env
           ]
       ]
