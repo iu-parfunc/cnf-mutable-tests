acc5457aba91e9423f13878942ad78f049286da8
Vikraman Choudhury <git@vikraman.org>
2016-05-31 19:37:14 -0400

    Evaluate to nf in for_

--- bench/Utils.hs
+++ bench/Utils.hs
@@ -2,12 +2,15 @@
 
 module Utils where
 
+import Control.DeepSeq
+import Control.Exception
+
 {-# INLINABLE for_ #-}
-for_ :: (Num n, Ord n, Monad m) => n -> n -> (n -> m a) -> m ()
+for_ :: (Num n, Ord n, NFData a) => n -> n -> (n -> IO a) -> IO ()
 for_ start end _
   | start > end = error "start greater than end"
 for_ start end fn = loop start
   where
     loop i
       | i > end = return ()
-      | otherwise = fn i >> loop (i + 1)
+      | otherwise = fn i >>= (evaluate . rnf) >> loop (i + 1)
