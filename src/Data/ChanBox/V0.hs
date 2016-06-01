9b3d6a9d99f28026a74e584c0d57fa4695bc9fb0
Vikraman Choudhury <git@vikraman.org>
2016-05-31 23:12:25 -0400

    Some performance optimizations

--- src/Data/ChanBox/V0.hs
+++ src/Data/ChanBox/V0.hs
@@ -7,10 +7,11 @@
 module Data.ChanBox.V0 where
 
 import Control.DeepSeq
-import Control.Monad               as M
+import Control.Monad
+import Control.Monad.Utils
 import Data.IORef
-import Data.Vector                 as B
-import Data.Vector.Unboxed.Mutable as V
+import Data.Vector                 as V
+import Data.Vector.Unboxed.Mutable as VU
 import GHC.Generics
 
 type Msg = IOVector Int
@@ -26,9 +27,11 @@ data ChanBox = ChanBox { box :: Chan }
 instance NFData ChanBox where
   rnf ChanBox { .. } = rnf box
 
+{-# INLINE newMessage #-}
 newMessage :: ChanBox -> Int -> IO Msg
-newMessage _ = V.replicate 1024
+newMessage _ = VU.replicate 1024
 
+{-# INLINE newBox #-}
 newBox :: IO ChanBox
 newBox = newBox' 2000
 
@@ -36,8 +39,7 @@ newBox' :: Int -> IO ChanBox
 newBox' maxSize = do
   front <- newIORef 0
   rear <- newIORef 0
-  msg <- V.replicate 1024 0
-  let vec = B.replicate (maxSize + 1) msg
+  vec <- V.replicateM (maxSize + 1) (VU.replicate 1024 0)
   let box = Chan vec front rear
   return $ ChanBox box
 
@@ -45,20 +47,22 @@ lengthChan :: Chan -> IO Int
 lengthChan Chan { .. } = do
   start <- readIORef front
   end <- readIORef rear
-  let maxSize = B.length vec
+  let maxSize = V.length vec
   return $ if end >= start
              then end - start
              else end - start + maxSize
 
+{-# INLINE sizeBox #-}
 sizeBox :: ChanBox -> IO Int
 sizeBox ChanBox { .. } = lengthChan box
 
+{-# INLINE dropMinChan #-}
 dropMinChan :: ChanBox -> IO ()
 dropMinChan ChanBox { .. } =
   case box of
     Chan { .. } -> do
       start <- readIORef front
-      let maxSize = B.length vec
+      let maxSize = V.length vec
           start' = (start + 1) `mod` maxSize
       writeIORef front start'
 
@@ -67,12 +71,12 @@ pushMsg b@ChanBox { .. } msg =
   case box of
     Chan { .. } -> do
       size <- lengthChan box
-      let maxSize = B.length vec
+      let maxSize = V.length vec
       when (size == maxSize - 1) $ dropMinChan b
       end <- readIORef rear
       let end' = (end + 1) `mod` maxSize
           msg' = vec ! end
-      M.forM_ [0 .. 1023] $ \i -> do
-        a <- V.read msg i
-        V.write msg' i a
+      for_ 0 1023 $ \i -> do
+        a <- unsafeRead msg i
+        unsafeWrite msg' i a
       writeIORef rear end'
--- src/Data/ChanBox/V1.hs
+++ src/Data/ChanBox/V1.hs
@@ -12,13 +12,14 @@
 module Data.ChanBox.V1 where
 
 import Control.DeepSeq
-import Control.Monad               as M
+import Control.Monad
 import Control.Monad.Trans
+import Control.Monad.Utils
 import Data.CNFRef
 import Data.CNFRef.DeepStrict
 import Data.Compact.Indexed
-import Data.Vector                 as B
-import Data.Vector.Unboxed.Mutable as V
+import Data.Vector                 as V
+import Data.Vector.Unboxed.Mutable as VU
 import GHC.Generics
 
 type Msg = IOVector Int
@@ -36,9 +37,11 @@ data ChanBox = forall s. ChanBox { box :: Chan s }
 instance NFData ChanBox where
   rnf ChanBox { .. } = rnf box
 
+{-# INLINE newMessage #-}
 newMessage :: ChanBox -> Int -> IO Msg
-newMessage _ = V.replicate 1024
+newMessage _ = VU.replicate 1024
 
+{-# INLINE newBox #-}
 newBox :: IO ChanBox
 newBox = newBox' 2000
 
@@ -46,8 +49,7 @@ newBox' :: Int -> IO ChanBox
 newBox' maxSize = runCIO $ do
   front <- newCNFRef 0
   rear <- newCNFRef 0
-  msg <- V.replicate 1024 0
-  let vec = B.replicate (maxSize + 1) msg
+  vec <- V.replicateM (maxSize + 1) (VU.replicate 1024 0)
   c <- liftIO $ newCompactIn front vec
   let box = Chan c front rear
   return $ ChanBox box
@@ -56,20 +58,22 @@ lengthChan :: Chan s -> IO Int
 lengthChan Chan { .. } = do
   start <- getCompact <$> readCNFRef front
   end <- getCompact <$> readCNFRef rear
-  let maxSize = B.length (getCompact vec)
+  let maxSize = V.length (getCompact vec)
   return $ if end >= start
              then end - start
              else end - start + maxSize
 
+{-# INLINE sizeBox #-}
 sizeBox :: ChanBox -> IO Int
 sizeBox ChanBox { .. } = lengthChan box
 
+{-# INLINE dropMinChan #-}
 dropMinChan :: ChanBox -> IO ()
 dropMinChan ChanBox { .. } =
   case box of
     Chan { .. } -> do
       start <- getCompact <$> readCNFRef front
-      let maxSize = B.length (getCompact vec)
+      let maxSize = V.length (getCompact vec)
           start' = (start + 1) `mod` maxSize
       c <- newCompactIn front start'
       writeCNFRef front c
@@ -79,16 +83,16 @@ pushMsg b@ChanBox { .. } msg =
   case box of
     Chan { .. } -> do
       size <- lengthChan box
-      let maxSize = B.length (getCompact vec)
+      let maxSize = V.length (getCompact vec)
       when (size == maxSize - 1) $ dropMinChan b
       end <- getCompact <$> readCNFRef rear
       let end' = (end + 1) `mod` maxSize
           v = getCompact vec
           msg' = v ! end
-      M.forM_ [0 .. 1023] $ \i -> do
-        a <- V.read msg i
+      for_ 0 1023 $ \i -> do
+        a <- unsafeRead msg i
         c <- appendCompactNoShare vec a
         -- FIXME: unsafe interface
-        V.write msg' i (getCompact c)
+        unsafeWrite msg' i (getCompact c)
       c <- newCompactIn rear end'
       writeCNFRef rear c
