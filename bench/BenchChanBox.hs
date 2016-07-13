b5940133cb81473110a27e34592e3729f09a3140
Vikraman Choudhury <git@vikraman.org>
2016-07-12 22:53:07 -0400

    Refactor the benchmarks again

--- bench/BenchChanBox.hs
+++ bench/BenchChanBox.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Strict              #-}
+{-# LANGUAGE StrictData          #-}
 
 module Main where
 
@@ -15,21 +17,27 @@ import qualified Data.Vector                 as V
 import qualified Data.Vector.Unboxed         as VU
 import           GHC.Generics
 import           Network.HostName
-import           System.Console.CmdArgs      as CA
 import           System.Environment
+import           System.IO.Unsafe
 import           System.Random.PCG.Fast.Pure as PCG
 
 import qualified Data.ChanBox.V0 as CB0
 import qualified Data.ChanBox.V1 as CB1
 import qualified Data.ChanBox.V2 as CB2
 
-data Flags = Flags { size       :: Int
-                   , threads    :: Int
-                   , range      :: Int
-                   , randomSize :: Int
-                   , criterion  :: [String]
-                   }
-  deriving (Eq, Show, Data, Typeable, Generic, NFData)
+{-# NOINLINE size #-}
+size :: Int
+size = unsafePerformIO $ read <$> getEnv "SIZE"
+
+{-# NOINLINE threads #-}
+threads :: Int
+threads = unsafePerformIO $ read <$> getEnv "THREADS"
+
+range :: Int
+range = maxBound
+
+randomSize :: Int
+randomSize = 10 ^ 7
 
 data Env = Env { randomInts :: VU.Vector Int
                , cb0        :: CB0.ChanBox
@@ -41,17 +49,8 @@ data Env = Env { randomInts :: VU.Vector Int
                }
   deriving (Generic, NFData)
 
-defaultFlags :: Flags
-defaultFlags = Flags
-  { size = (10 ^ 2) &= name "size" &= help "Maximum size of ChanBox"
-  , threads = 4 &= name "threads" &= help "Number of threads for the concurrent benchmark"
-  , range = (2 ^ 63 - 1) &= help "Integer range for messages"
-  , randomSize = (10 ^ 7) &= help "Size of precomputed random vector"
-  , criterion = [] &= help "Arguments to criterion"
-  } &= CA.verbosity &= program "bench-chanbox"
-
-setupEnv :: Flags -> IO Env
-setupEnv Flags { .. } = do
+setupEnv :: IO Env
+setupEnv = do
   gen <- PCG.createSystemRandom
   randomInts <- VU.replicateM randomSize (uniformR (0, range) gen :: IO Int)
   cb0 <- CB0.newBox size
@@ -73,45 +72,45 @@ getConfig = do
     , csvFile = Just $ filename ++ ".csv"
     }
 
-cbv0Bench :: Flags -> Env -> Benchmarkable
-cbv0Bench Flags { .. } Env { .. } = Benchmarkable $
+cbv0Bench :: Env -> Benchmarkable
+cbv0Bench Env { .. } = Benchmarkable $
   \n -> for_ 0 (fromIntegral n) $ \i -> do
     let cb = cb0
     msg <- CB0.newMessage cb $ randomInts VU.! (i `mod` randomSize)
     CB0.pushMsg cb msg
 
-cbv1Bench :: Flags -> Env -> Benchmarkable
-cbv1Bench Flags { .. } Env { .. } = Benchmarkable $
+cbv1Bench :: Env -> Benchmarkable
+cbv1Bench Env { .. } = Benchmarkable $
   \n -> for_ 1 (fromIntegral n) $ \i -> do
     let cb = cb1
     msg <- CB1.newMessage cb $ randomInts VU.! (i `mod` randomSize)
     CB1.pushMsg cb msg
 
-cbv2Bench :: Flags -> Env -> Benchmarkable
-cbv2Bench Flags { .. } Env { .. } = Benchmarkable $
+cbv2Bench :: Env -> Benchmarkable
+cbv2Bench Env { .. } = Benchmarkable $
   \n -> for_ 1 (fromIntegral n) $ \i -> do
     let cb = cb2
     msg <- CB2.newMessage cb $ randomInts VU.! (i `mod` randomSize)
     CB2.pushMsg cb msg
 
-cbv0ConcBench :: Flags -> Env -> Benchmarkable
-cbv0ConcBench Flags { .. } Env { .. } = Benchmarkable $
+cbv0ConcBench :: Env -> Benchmarkable
+cbv0ConcBench Env { .. } = Benchmarkable $
   \n -> forConc_ 0 (threads - 1) $ \t -> do
     let cb = cb0s V.! t
     for_ 0 (fromIntegral n) $ \i -> do
       msg <- CB0.newMessage cb $ randomInts VU.! (i `mod` randomSize)
       CB0.pushMsg cb msg
 
-cbv1ConcBench :: Flags -> Env -> Benchmarkable
-cbv1ConcBench Flags { .. } Env { .. } = Benchmarkable $
+cbv1ConcBench :: Env -> Benchmarkable
+cbv1ConcBench Env { .. } = Benchmarkable $
   \n -> forConc_ 0 (threads - 1) $ \t -> do
     let cb = cb1s V.! t
     for_ 0 (fromIntegral n) $ \i -> do
       msg <- CB1.newMessage cb $ randomInts VU.! (i `mod` randomSize)
       CB1.pushMsg cb msg
 
-cbv2ConcBench :: Flags -> Env -> Benchmarkable
-cbv2ConcBench Flags { .. } Env { .. } = Benchmarkable $
+cbv2ConcBench :: Env -> Benchmarkable
+cbv2ConcBench Env { .. } = Benchmarkable $
   \n -> forConc_ 0 (threads - 1) $ \t -> do
     let cb = cb2s V.! t
     for_ 0 (fromIntegral n) $ \i -> do
@@ -120,19 +119,17 @@ cbv2ConcBench Flags { .. } Env { .. } = Benchmarkable $
 
 main :: IO ()
 main = do
-  flags@Flags { .. } <- cmdArgs defaultFlags
   config <- getConfig
-  withArgs criterion $
-    defaultMainWith config
-      [ env (setupEnv flags) $ \env ->
-        bgroup "ChanBox"
-          [ bench "V0" $ cbv0Bench flags env
-          , bench "V1" $ cbv1Bench flags env
-          , bench "V2" $ cbv2Bench flags env
-          , bgroup "Concurrent"
-              [ bench "V0" $ cbv0ConcBench flags env
-              , bench "V1" $ cbv1ConcBench flags env
-              , bench "V2" $ cbv2ConcBench flags env
-              ]
-          ]
-      ]
+  defaultMainWith config
+    [ env setupEnv $ \ ~env ->
+      bgroup "ChanBox"
+        [ bench "V0" $ cbv0Bench env
+        , bench "V1" $ cbv1Bench env
+        , bench "V2" $ cbv2Bench env
+        , bgroup "Concurrent"
+            [ bench "V0" $ cbv0ConcBench env
+            , bench "V1" $ cbv1ConcBench env
+            , bench "V2" $ cbv2ConcBench env
+            ]
+        ]
+    ]
